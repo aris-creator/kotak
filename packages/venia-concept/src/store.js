magento/pwa-studio

feat(scaffolding): npm init @magento/pwa or yarn create @magento/pwa
feat(config): add webpack tool for env generation and management

fix(config): backwards compat env vars support

feat(scaffold): create-pwa first draft

WIP compat and esm

fixup: fix build:analyze script

WIP fixing build

refactor: Changes for scaffolding portability

fixup rename sample backends camelcased

Revert "refactor: improve driver portability for consuming venia in another app"

This reverts commit d309335297bac730b54fde2c86da5181c157b933.

packages/venia-concept/src/store.js
@@ -1,8 +1,8 @@
 import { createStore } from 'redux';
 
-import middleware from 'src/middleware';
-import errorHandler from 'src/middleware/errorHandler';
-import reducer from 'src/reducers';
-import composeEnhancers from 'src/util/composeEnhancers';
+import middleware from '@magento/venia-library/esm/middleware';
+import errorHandler from '@magento/venia-library/esm/middleware/errorHandler';
+import reducer from '@magento/venia-library/esm/reducers';
+import composeEnhancers from '@magento/venia-library/esm/util/composeEnhancers';
 
 export default createStore(reducer, composeEnhancers(middleware, errorHandler));

aris-creator/kotak

packages/venia-concept/src/store.js
@@ -1,8 +1,8 @@
 import { createStore } from 'redux';
 
-import middleware from 'src/middleware';
-import errorHandler from 'src/middleware/errorHandler';
-import reducer from 'src/reducers';
-import composeEnhancers from 'src/util/composeEnhancers';
+import middleware from '@magento/venia-library/middleware';
+import errorHandler from '@magento/venia-library/middleware/errorHandler';
+import reducer from '@magento/venia-library/reducers';
+import composeEnhancers from '@magento/venia-library/util/composeEnhancers';
 
 export default createStore(reducer, composeEnhancers(middleware, errorHandler));