b9d87052615193568a48a32133f527d026649e68
Jimmy Sanford <jimbo@users.noreply.github.com>
2020-10-23 10:22:49 -0700

    Demo react-select

--- packages/venia-concept/local-intercept.js
+++ packages/venia-concept/local-intercept.js
@@ -1,22 +1,20 @@
 /* eslint-disable */
-/**
- * Custom interceptors for the project.
- *
- * This project has a section in its package.json:
- *    "pwa-studio": {
- *        "targets": {
- *            "intercept": "./local-intercept.js"
- *        }
- *    }
- *
- * This instructs Buildpack to invoke this file during the intercept phase,
- * as the very last intercept to run.
- *
- * A project can intercept targets from any of its dependencies. In a project
- * with many customizations, this function would tap those targets and add
- * or modify functionality from its dependencies.
- */
 
-function localIntercept() {}
+function localIntercept(targets) {
+    const { Targetables } = require('@magento/pwa-buildpack');
+    const targetables = Targetables.using(targets);
+
+    const Select = targetables.reactComponent(
+        '@magento/venia-ui/lib/components/Select/select.js'
+    );
+    const FancySelect = Select.addImport(
+        "FancySelect from '@magento/venia-concept/src/components/FancySelect'"
+    );
+
+    Select.replaceJSX(
+        'FieldIcons',
+        `<${FancySelect} {...rest} fieldState={fieldState} options={items} />`
+    );
+}
 
 module.exports = localIntercept;
---
+++ packages/venia-concept/src/components/FancySelect/fancySelect.js
@@ -0,0 +1,47 @@
+import React, { useCallback, useMemo } from 'react';
+import Select from 'react-select';
+
+const FancySelect = props => {
+    const { disabled, fieldApi, fieldState, forwardedRef, options } = props;
+    const { setValue, setTouched } = fieldApi;
+
+    const handleBlur = useCallback(() => {
+        setTouched(true);
+    }, [setTouched]);
+
+    const handleChange = useCallback(
+        nextValue => {
+            setValue(nextValue.value);
+        },
+        [setValue]
+    );
+
+    const value = useMemo(() => {
+        const { value: rawValue } = fieldState || {};
+
+        return rawValue || rawValue === 0 ? rawValue : '';
+    }, [fieldState]);
+
+    return (
+        <Select
+            ref={forwardedRef}
+            disabled={disabled}
+            onBlur={handleBlur}
+            onChange={handleChange}
+            options={options}
+            styles={styles}
+            value={value}
+        />
+    );
+};
+
+export default FancySelect;
+
+const styles = {
+    control: base => ({
+        ...base,
+        borderColor: 'rgb(var(--venia-global-color-gray-600))',
+        borderRadius: 6,
+        borderWidth: 2
+    })
+};
